1f626c96e1484631fc8f43ccf46da6719ad99c22
Adam O'Grady <adam.ogrady@gmail.com>
2020-02-25 20:29:59 +0800

    fixing spider to work with Celery, fixing dockerfile to use spider

--- indexer/spider.py
+++ indexer/spider.py
@@ -1,3 +1,6 @@
+from celery import Celery
 import scrape
 
-scrape.scrape_and_spider.delay("http://protonirockerxow.onion")
+app = Celery('tasks', broker='amqp://guest:guest@rabbitmq:5672')
+
+scrape.scrape_and_spider.delay("http://3g2upl4pq6kufc4m.onion/")
